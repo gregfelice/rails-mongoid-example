8426dec27a5cdc0241e376668067e08e8c5b0fdc
Greg Felice <gregfelice@gmail.com>
2014-12-09 19:01:04 -0800

    lots of changes

--- lago/test/models/post_test.rb
+++ lago/test/models/post_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "the truth" do
+    assert true
+  end
+
+  test "general stuff about the post model" do 
+
+    
+    post = Post.new(:title => "title")
+    post.save
+
+    posts = Post.all
+
+    puts posts.size
+
+    posts.each {|post| 
+      
+      puts post.inspect
+    }
+
+  end
+
 end
